cc202cc5b04e5e8fc1a5d76b00680069a81c38b1
moris96 <93961035+moris96@users.noreply.github.com>
2022-04-28 11:05:07 -0400

    finished app

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # phone-number-tracker is in development 
+
+### pip install phonenumbers ; pip install opencage 
+
+#### enter a mobile number with the country code like +1 (USA) at the beginning
+
+#### to search for the coordinates on a map, just copy-paste them to google maps 
+
+#### for an API key, sign up for [opencage](https://opencagedata.com/). It's free so don't worry about paying for an API Key
---
+++ locateNumber.py
@@ -0,0 +1,35 @@
+import phonenumbers
+from phonenumbers import carrier, geocoder, timezone
+from opencage.geocoder import OpenCageGeocode
+
+number = input('Enter a mobile number with country code: ')
+number = phonenumbers.parse(number)
+
+#geocoder API key for lat & lon coordinates
+key = '1a6118c1609e49adb09e7cb3119d6909'
+
+
+#US state where the number is from, or province and etc for non-US countries
+country_location = geocoder.description_for_number(number, "en")
+print(country_location)
+
+#timezone
+print(timezone.time_zones_for_number(number))
+
+#carrier
+print(carrier.name_for_number(number, "en"))
+
+#making sure it's a valid number
+print("Valid movile number: ", phonenumbers.is_valid_number(number))
+
+
+#lat & lon coordinates for number
+geocoder = OpenCageGeocode(key)
+
+query = str(country_location)
+
+results = geocoder.geocode(query)
+
+lat = results[0]['geometry']['lat']
+lon = results[0]['geometry']['lng']
+print(lat, lon)
